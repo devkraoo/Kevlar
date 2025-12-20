04f38258395c622dd47ece314a30b6552a75205e
joseph <joseph96501@gmail.com>
2025-12-19 20:13:12 -0500

    Made SyntaxBinding.kt a data class ParsedSyntax.kt represents Syntax in an executable state

--- src/main/kotlin/kommand/syntax/Syntax.kt
+++ src/main/kotlin/kommand/syntax/Syntax.kt
@@ -7,10 +7,9 @@ import kommand.arguments.Arguments
 class Syntax<A: Arguments> internal constructor(
 	private val holder: A
 ) {
-	val arguments: List<ArgumentDelegate<*>>
-		get() = holder.delegates
+	val arguments = holder.delegates
 
-	fun execute(executor: ArgumentExecutor<A>, context: KommandContext) {
-		with(context) { holder.executor() }
+	fun execute(executor: ArgumentExecutor<A>, context: KommandContext) = with(context) {
+		holder.executor()
 	}
 }
--- src/main/kotlin/kommand/syntax/SyntaxBinding.kt
+++ src/main/kotlin/kommand/syntax/SyntaxBinding.kt
@@ -1,13 +1,8 @@
 package kommand.syntax
 
-import kommand.KommandContext
 import kommand.arguments.Arguments
 
 data class SyntaxBinding<A: Arguments>(
 	val syntax: Syntax<A>,
 	val executor: ArgumentExecutor<A>
-) {
-	fun execute(context: KommandContext) {
-		syntax.execute(executor, context)
-	}
-}
+)
